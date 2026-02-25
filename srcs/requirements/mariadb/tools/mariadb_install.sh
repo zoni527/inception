f57422d431c189dbab71debf193ec336c68d6da3
Johnny Varila <jvarila@student.hive.fi>
2026-02-25 17:00:34 +0200

    Small progress

--- srcs/requirements/mariadb/tools/mariadb_install.sh
+++ srcs/requirements/mariadb/tools/mariadb_install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-mariadb-install-db --user=$MARIADB_USER --basedir=./ --datadir=./data
 
+# Environment variable controlling the port that is used for
+# connecting to mariadb
+export MYSQL_TCP_PORT=$MARIADB_PORT_INTERNAL
+
+mariadb-install-db --user=mysql --basedir=/usr
+mariadbd --user=mysql
