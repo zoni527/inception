c22969d82e5fc30e3de469ff8d06e4474238ef91
Johnny Varila <jvarila@student.hive.fi>
2026-02-26 18:31:21 +0200

    Extending the mariadb install script

--- srcs/requirements/mariadb/tools/mariadb_install.sh
+++ srcs/requirements/mariadb/tools/mariadb_install.sh
@@ -4,5 +4,50 @@
 # connecting to mariadb
 export MYSQL_TCP_PORT=$MARIADB_PORT_INTERNAL
 
-mariadb-install-db --user=mysql --basedir=/usr
-mariadbd --user=mysql
+user=mysql
+root_password=rootpwd # $(cat $MARIADB_ROOT_PASSWORD_FILE)
+datadir=/var/lib/mysql
+logdir=/var/log/mysql
+log_error=$logdir/mariadb.err
+
+# If tables are already present it means that the script has already been run
+if [ -d $datadir/data ]; then
+	mariadb-safe;
+	exit 1; # Safety exit if above command exits unexpectedly
+fi
+
+if [ ! "$root_password" ]; then
+	echo "Empty mariadb root password, exiting"
+	exit 1;
+fi
+
+# If user doesn't exist create it and add it to the mysql group
+if [ ! "$(grep $user /etc/passwd)" ]; then
+	adduser -H $user && adduser -G $user mysql
+fi;
+echo "Creating directories"
+mkdir -p $datadir $logdir && chown $user $datadir $logdir
+
+# Run install for initialization
+mariadb-install-db \
+	--user=$user \
+	--basedir=/usr \
+	--datadir=$datadir \
+	--log-error=$log_error \
+	--skip-test-db
+
+# Start daemon temporarily for setup
+echo "Setup start"
+mariadbd \
+	--user=$user \
+	--datadir=$datadir \
+	--log-error=$log_error \
+	--port=$MYSQL_TCP_PORT \
+ 	--bootstrap <<- EOF
+ 	-- Disable root login without password and set password
+ 	GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'rootpwd' WITH GRANT OPTION;
+ 	FLUSH PRIVILEGES;
+EOF
+
+echo "Running mariadb"
+exec mariadbd-safe --user=$user --datadir=$datadir --log-error=$log_error --port=$MYSQL_TCP_PORT
